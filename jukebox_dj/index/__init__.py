77d5f04e293a442a0ee05a916bcce08807db1819
Jake Harding <jacobeharding@gmail.com>
2017-09-16 21:47:47 -0500

    Remove default app confing in index init.

--- jukebox_dj/index/__init__.py
+++ jukebox_dj/index/__init__.py
@@ -1,12 +1,1 @@
-"""
-__init__.py - (C) Copyright - 2017
 
-SPDX-License-Identifier: SEE LICENSE.txt
-
-Author(s) of this file:
-  J. Harding
-
-Setup app config
-"""
-
-default_app_config = 'jukebox_dj.index.apps.AppConfig'
