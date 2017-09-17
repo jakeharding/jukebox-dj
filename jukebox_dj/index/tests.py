dd14b4431fc716ebc455f0efcb9260d02d1fb71b
Jake Harding <jacobeharding@gmail.com>
2017-09-16 20:06:34 -0500

    Update requirements and remove some unneeded boilerplate.

--- jukebox_dj/index/tests.py
+++ jukebox_dj/index/tests.py
@@ -1,8 +1,21 @@
+"""
+tests.py - (C) Copyright - 2017
+
+SPDX-License-Identifier: SEE LICENSE.txt
+
+Author(s) of this file:
+  J. Harding
+
+Test around the index view.
+"""
+
 from django.test import TestCase
+
+from rest_framework.status import HTTP_200_OK
 
 
 class TestIndexView(TestCase):
 
     def testView(self):
         r = self.client.get("/")
-        self.assertTrue(r.status_code is 200)
+        self.assertTrue(r.status_code is HTTP_200_OK)
