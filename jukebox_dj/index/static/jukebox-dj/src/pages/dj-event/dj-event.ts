d7c7ed914eaf21addf53a51eda7a79e2f90a99b3
Jake Harding <jacobeharding@gmail.com>
2017-09-27 21:00:52 -0500

    Comment out POC code. Make request to get event data.

--- jukebox_dj/index/static/jukebox-dj/src/pages/dj-event/dj-event.ts
+++ jukebox_dj/index/static/jukebox-dj/src/pages/dj-event/dj-event.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {HomePage} from "../home/home";
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 /**
  * Generated class for the DjEventPage page.
@@ -18,13 +19,30 @@ import {HomePage} from "../home/home";
   templateUrl: 'dj-event.html',
 })
 export class DjEventPage {
-  home = HomePage;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log(navParams);
+  event: any; //TODO Model event into a class and remove any
+  requests: any[]; // TODO Model songs and song requests and remove any
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private http: Http
+  ) {
+
+    this.event = navParams.data;
+
+    // Quick and dirty http request. Plenty of TODOs
+    // TODO MUST - Hold the api version (dev in the url) in an environment specific way
+    // TODO MUST - Hold the url string in a symbolic constant
+    // TODO MUST - Model events and song requests into classes to remove the any
+    // TODO Stretch - Use the ngrx store to store the state of objects and handle requests
+    this.http.get(`/api/dev/events/${this.event.uuid}`)
+      .map(res => res.json())
+      .subscribe(data => {
+        this.requests = data.song_requests;
+      })
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DjEventPage');
   }
 
 }
