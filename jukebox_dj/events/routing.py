c001e3669d0c3f16644035a445494818f73aaeac
Jake Harding <jacobeharding@gmail.com>
2017-10-12 23:31:28 -0500

    Remove prototype and unused routes.

--- jukebox_dj/events/routing.py
+++ jukebox_dj/events/routing.py
@@ -3,10 +3,6 @@ from channels import route_class
 from .consumers import EventConsumer, RequesterConsumer
 
 channel_routing = [
-    # route_class(EventConsumer, path=r"^/events/(?P<uuid>\w{8}-\w{4}-\w{"
-    #                                 r"4}-\w{4}-\w{12})$"),
-    # route_class(EventConsumer, path=r"^/events/(?P<uuid>+\w{8}-\w{4}-\w{"
-    #                                 r"4}-\w{4}-\w{12}/requester/(?P<session>+\w)$")
     EventConsumer.as_route(path=r"^/events/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})$"),
-    # RequesterConsumer.as_route(path=r"^/events/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/requester/(?P<session>\w+)$"),
+    RequesterConsumer.as_route(path=r"^/events/(?P<uuid>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/requester/(?P<session>\w+)$"),
 ]
