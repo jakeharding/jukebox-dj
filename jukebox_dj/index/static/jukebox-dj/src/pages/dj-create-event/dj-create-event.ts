6e36fb531cc1e095ff5963f7d2fa64d1fc17b5c1
Tristan <tristanmartin2016@gmail.com>
2017-11-09 11:51:06 -0600

    Created dj-create-event page

--- jukebox_dj/index/static/jukebox-dj/src/pages/dj-create-event/dj-create-event.ts
+++ jukebox_dj/index/static/jukebox-dj/src/pages/dj-create-event/dj-create-event.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EventProvider } from "../../providers/event/event";
+import { Event } from '../../models/Event';
 
 /**
  * Generated class for the DjCreateEventPage page.
@@ -17,12 +19,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'dj-create-event.html',
 })
 export class DjCreateEventPage {
+  active: boolean = false;
+
+  eventName: string = '';
+  eventNameCorrect: boolean = true;
+
+  event: Event;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad DjCreateEventPage');
+  verifyInput(eventName: string) {
+    if (eventName == '') {
+      this.eventNameCorrect = false;
+      return;
+    }
+    this.eventNameCorrect = true;
+  }
+
+  createEvent(event: Event) {
+
   }
 
 }
