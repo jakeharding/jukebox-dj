81a1f7eacb4961713fa62a5d11d3bde3adf6eba0
jpwitkowsky <jpwitkowsky@unomaha.edu>
2017-11-13 16:52:15 -0600

    started functionality with events button

--- jukebox_dj/index/static/jukebox-dj/src/pages/home/home.ts
+++ jukebox_dj/index/static/jukebox-dj/src/pages/home/home.ts
@@ -11,23 +11,26 @@ import { EventProvider} from "../../providers/event/event";
   templateUrl: 'home.html'
 })
 export class HomePage {
+  noEventFound: boolean = false;
   djId: string;
   event: Event;
   events: Event[] = [];
 
 
   constructor(public navCtrl: NavController, private eventProvider: EventProvider) {
-
+    this.noEventFound = false;
   }
-
   navigate(){
-    this.eventProvider.getEvents({dj_id:this.djId,is_active:true}).subscribe(events => {
-      if(events.length === 1){
-        this.navCtrl.push("requester",events[0])
+    this.eventProvider.getEvents({dj__dj_id:this.djId,is_active:true}).subscribe(events => {
+      this.noEventFound = false;
+      if(events.length === 1) {
+        this.navCtrl.push("requester", events[0])
+      } else if (events.length > 1){
+        this.events = events;
+      } else {
+        this.noEventFound = true;
+        this.events = [];
       }
-      //  this.events = events;
-    })
-    console.log(this.djId);
-    //this.navCtrl.push(Event);
+    });
   }
 }
