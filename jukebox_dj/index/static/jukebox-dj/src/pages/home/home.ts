c7271fd9535e9da86e5e5029a917b9c33d7b0641
jpwitkowsky <jpwitkowsky@unomaha.edu>
2017-11-13 16:06:25 -0600

    Home screen update. Handled one active event case

--- jukebox_dj/index/static/jukebox-dj/src/pages/home/home.ts
+++ jukebox_dj/index/static/jukebox-dj/src/pages/home/home.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { Event } from '../../models/Event';
 import { EventProvider} from "../../providers/event/event";
-import {User} from "../../models/User";
 
 @IonicPage({
   name: 'home'
@@ -12,19 +11,23 @@ import {User} from "../../models/User";
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  user: User;
+  djId: string;
   event: Event;
   events: Event[] = [];
 
 
-  constructor(private eventProvider: EventProvider) {
-    eventProvider.getEvents({}).subscribe(events => {
-      this.events = events;
-    })
+  constructor(public navCtrl: NavController, private eventProvider: EventProvider) {
+
   }
 
-  userEvent (user: User) {
-    this.user = user;
+  navigate(){
+    this.eventProvider.getEvents({dj_id:this.djId,is_active:true}).subscribe(events => {
+      if(events.length === 1){
+        this.navCtrl.push("requester",events[0])
+      }
+      //  this.events = events;
+    })
+    console.log(this.djId);
+    //this.navCtrl.push(Event);
   }
 }
